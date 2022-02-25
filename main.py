bce64046b58cb92c973ff9061171ded4f9f3568b
VladimirVKA <94992184+VladimirVKA@users.noreply.github.com>
2022-02-25 15:09:17 +0400

    написал @property

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-# создать класс, два поля интренал, прайвад и проперти к прайвд
+# создать класс, два поля интренал, прайвeд и проперти к прайвд
 # залить на репозиторий
 class gg:
+    __name: str
+    _surname:str
     def __init__(self):
-        self._name = ''
-        self._sername = ''
+        self.__name = ''
+        self._surname = ''
 
-    def getname(self, name:str):
-        self._name = name
-    def getsername(self, sername:str):
-        self._sername = sername
-    name = property(getname,getsername)
+
+    @property
+    def getname(self):
+        return self.__name
+    @getname.setter
+    def getname(self, name = 'Ivan'):
+        self.__name = name
+
+    @property
+    def getsurname(self):
+        return self._surname
+    def gersurname(self, sername = 'Ivanov'):
+        self._surname = sername
+
+if __name__ == '__main__':
+    x = gg()
+
+
